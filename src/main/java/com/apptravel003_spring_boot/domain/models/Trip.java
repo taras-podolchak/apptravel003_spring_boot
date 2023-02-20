75cf88c46c22c0724c082d267f4d472800c98b28
Taras <taraspodolchak@gmail.com>
2023-02-20 16:53:12 +0100

    added GitHub Actions #6

--- src/main/java/com/apptravel003_spring_boot/domain/models/Trip.java
+++ src/main/java/com/apptravel003_spring_boot/domain/models/Trip.java
@@ -1,9 +1,18 @@
 package com.apptravel003_spring_boot.domain.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trip {
-
     private Integer typeTrip;
     private Integer statusTrip;
     private String title;
@@ -12,162 +21,11 @@ public class Trip {
     private Integer transportType;
     private Integer price;
     private String general;
-    private Integer recommendation;
-    private Integer safety;
+    private String recommendation;
+    private String safety;
     private Integer oneWayTrip;
     private Integer returnTrip;
     private List<Activity> activityList;
     private List<User> userList;
 
-    public Trip() {
-        //empty for framework
-    }
-
-
-    public Trip(Integer typeTrip, Integer statusTrip, String title, String photoTrip, Integer level, Integer transportType, Integer price, String general, Integer recommendation, Integer safety, Integer oneWayTrip, Integer returnTrip, List<Activity> activityList, List<User> userList) {
-        this.typeTrip = typeTrip;
-        this.statusTrip = statusTrip;
-        this.title = title;
-        this.photoTrip = photoTrip;
-        this.level = level;
-        this.transportType = transportType;
-        this.price = price;
-        this.general = general;
-        this.recommendation = recommendation;
-        this.safety = safety;
-        this.oneWayTrip = oneWayTrip;
-        this.returnTrip = returnTrip;
-        this.activityList = activityList;
-        this.userList = userList;
-    }
-
-    public Integer getTypeTrip() {
-        return typeTrip;
-    }
-
-    public void setTypeTrip(Integer typeTrip) {
-        this.typeTrip = typeTrip;
-    }
-
-    public Integer getStatusTrip() {
-        return statusTrip;
-    }
-
-    public void setStatusTrip(Integer statusTrip) {
-        this.statusTrip = statusTrip;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPhotoTrip() {
-        return photoTrip;
-    }
-
-    public void setPhotoTrip(String photoTrip) {
-        this.photoTrip = photoTrip;
-    }
-
-    public Integer getLevel() {
-        return level;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public Integer getTransportType() {
-        return transportType;
-    }
-
-    public void setTransportType(Integer transportType) {
-        this.transportType = transportType;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getGeneral() {
-        return general;
-    }
-
-    public void setGeneral(String general) {
-        this.general = general;
-    }
-
-    public Integer getRecommendation() {
-        return recommendation;
-    }
-
-    public void setRecommendation(Integer recommendation) {
-        this.recommendation = recommendation;
-    }
-
-    public Integer getSafety() {
-        return safety;
-    }
-
-    public void setSafety(Integer safety) {
-        this.safety = safety;
-    }
-
-    public Integer getOneWayTrip() {
-        return oneWayTrip;
-    }
-
-    public void setOneWayTrip(Integer oneWayTrip) {
-        this.oneWayTrip = oneWayTrip;
-    }
-
-    public Integer getReturnTrip() {
-        return returnTrip;
-    }
-
-    public void setReturnTrip(Integer returnTrip) {
-        this.returnTrip = returnTrip;
-    }
-
-    public List<Activity> getActivityList() {
-        return activityList;
-    }
-
-    public void setActivityList(List<Activity> activityList) {
-        this.activityList = activityList;
-    }
-
-    public List<User> getUserList() {
-        return userList;
-    }
-
-    public void setUserList(List<User> userList) {
-        this.userList = userList;
-    }
-
-    @Override
-    public String toString() {
-        return "Trip{" +
-                "typeTrip=" + typeTrip +
-                ", statusTrip=" + statusTrip +
-                ", title='" + title + '\'' +
-                ", photoTrip='" + photoTrip + '\'' +
-                ", level=" + level +
-                ", transportType=" + transportType +
-                ", price=" + price +
-                ", general='" + general + '\'' +
-                ", recommendation=" + recommendation +
-                ", safety=" + safety +
-                ", oneWayTrip=" + oneWayTrip +
-                ", returnTrip=" + returnTrip +
-                '}';
-    }
 }
--- src/main/java/com/apptravel003_spring_boot/domain/models/User.java
+++ src/main/java/com/apptravel003_spring_boot/domain/models/User.java
@@ -1,11 +1,20 @@
 package com.apptravel003_spring_boot.domain.models;
 
 
+import com.google.cloud.firestore.annotation.ServerTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
     private Integer typeUser;
     private Integer statusUser;
     private String name;
@@ -24,242 +33,11 @@ public class User {
     private String nationality;
     private String photoUser;
     private String photoOrganization;
+    @ServerTimestamp
     private Date entryDate;
+    @ServerTimestamp
     private Date leavingDate;
     private Boolean legalConditions;
     private Boolean rememberPassword;
 
-    public User() {
-        //empty for framework
-    }
-
-
-    public User(Integer typeUser, Integer statusUser, String name, String nickname, String surname, String secondSurname, List<Integer> preferActivity, Integer phoneNumber, String email, String country, String address, String location, Integer postalCode, Integer documentType, String documentNumber, String nationality, String photoUser, String photoOrganization, Date entryDate, Date leavingDate, Boolean legalConditions, Boolean rememberPassword) {
-        this.typeUser = typeUser;
-        this.statusUser = statusUser;
-        this.name = name;
-        this.nickname = nickname;
-        this.surname = surname;
-        this.secondSurname = secondSurname;
-        this.preferActivity = preferActivity;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.country = country;
-        this.address = address;
-        this.location = location;
-        this.postalCode = postalCode;
-        this.documentType = documentType;
-        this.documentNumber = documentNumber;
-        this.nationality = nationality;
-        this.photoUser = photoUser;
-        this.photoOrganization = photoOrganization;
-        this.entryDate = entryDate;
-        this.leavingDate = leavingDate;
-        this.legalConditions = legalConditions;
-        this.rememberPassword = rememberPassword;
-    }
-
-    public Integer getTypeUser() {
-        return typeUser;
-    }
-
-    public void setTypeUser(Integer typeUser) {
-        this.typeUser = typeUser;
-    }
-
-    public Integer getStatusUser() {
-        return statusUser;
-    }
-
-    public void setStatusUser(Integer statusUser) {
-        this.statusUser = statusUser;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getSecondSurname() {
-        return secondSurname;
-    }
-
-    public void setSecondSurname(String secondSurname) {
-        this.secondSurname = secondSurname;
-    }
-
-    public List<Integer> getPreferActivity() {
-        return preferActivity;
-    }
-
-    public void setPreferActivity(List<Integer> preferActivity) {
-        this.preferActivity = preferActivity;
-    }
-
-    public Integer getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Integer phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Integer getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(Integer postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public Integer getDocumentType() {
-        return documentType;
-    }
-
-    public void setDocumentType(Integer documentType) {
-        this.documentType = documentType;
-    }
-
-    public String getDocumentNumber() {
-        return documentNumber;
-    }
-
-    public void setDocumentNumber(String documentNumber) {
-        this.documentNumber = documentNumber;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getPhotoUser() {
-        return photoUser;
-    }
-
-    public void setPhotoUser(String photoUser) {
-        this.photoUser = photoUser;
-    }
-
-    public String getPhotoOrganization() {
-        return photoOrganization;
-    }
-
-    public void setPhotoOrganization(String photoOrganization) {
-        this.photoOrganization = photoOrganization;
-    }
-
-    public Date getEntryDate() {
-        return entryDate;
-    }
-
-    public void setEntryDate(Date entryDate) {
-        this.entryDate = entryDate;
-    }
-
-    public Date getLeavingDate() {
-        return leavingDate;
-    }
-
-    public void setLeavingDate(Date leavingDate) {
-        this.leavingDate = leavingDate;
-    }
-
-    public Boolean getLegalConditions() {
-        return legalConditions;
-    }
-
-    public void setLegalConditions(Boolean legalConditions) {
-        this.legalConditions = legalConditions;
-    }
-
-    public Boolean getRememberPassword() {
-        return rememberPassword;
-    }
-
-    public void setRememberPassword(Boolean rememberPassword) {
-        this.rememberPassword = rememberPassword;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "typeUser=" + typeUser +
-                ", statusUser=" + statusUser +
-                ", name='" + name + '\'' +
-                ", nickname='" + nickname + '\'' +
-                ", surname='" + surname + '\'' +
-                ", secondSurname='" + secondSurname + '\'' +
-                ", preferActivity=" + preferActivity +
-                ", phoneNumber=" + phoneNumber +
-                ", email='" + email + '\'' +
-                ", country='" + country + '\'' +
-                ", address='" + address + '\'' +
-                ", location='" + location + '\'' +
-                ", postalCode=" + postalCode +
-                ", documentType=" + documentType +
-                ", documentNumber='" + documentNumber + '\'' +
-                ", nationality='" + nationality + '\'' +
-                ", photoUser='" + photoUser + '\'' +
-                ", photoOrganization='" + photoOrganization + '\'' +
-                ", entryDate=" + entryDate +
-                ", leavingDate=" + leavingDate +
-                ", legalConditions=" + legalConditions +
-                ", rememberPassword=" + rememberPassword +
-                '}';
-    }
 }
